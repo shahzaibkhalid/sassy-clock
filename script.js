962238170999a8b397febf7487b0b12b25eb29e6
Shahzaib Khalid <shahzaib.khalid85@hotmail.com>
2017-09-20 12:58:55 +0500

    Add digital clock

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function syncDate() {
+    const hourHand = document.getElementById('hour-hand');
+    const minHand = document.getElementById('minute-hand');
+    const secHand = document.getElementById('second-hand');
+    const digitalClock = document.querySelector('.digital-clock');
+
     let now = new Date();
     let hour = now.getHours();
     let mins = now.getMinutes();
     let sec = now.getSeconds();
-    const hourHand = document.getElementById('hour-hand');
-    const minHand = document.getElementById('minute-hand');
-    const secHand = document.getElementById('second-hand');
     
     let hourInDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
     let minsInDegrees = ((mins / 60) * 360) + ((sec/60)*6) + 90;
@@ -14,6 +16,8 @@ function syncDate() {
     hourHand.style.transform = `rotate(${hourInDegrees}deg)`;
     minHand.style.transform = `rotate(${minsInDegrees}deg)`;
     secHand.style.transform = `rotate(${secInDegrees}deg)`;
+
+    digitalClock.innerHTML = `${hour}:${mins}:${sec}`;
 }
 
 setInterval(syncDate, 1000);
