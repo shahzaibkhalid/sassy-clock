9c06f1d44f332da61066d86c8cd725f224b1930b
Shahzaib Khalid <shahzaib.khalid85@hotmail.com>
2017-09-20 12:43:41 +0500

    Add basic structure of clock

---
+++ script.js
@@ -0,0 +1,19 @@
+function syncDate() {
+    let now = new Date();
+    let hour = now.getHours();
+    let mins = now.getMinutes();
+    let sec = now.getSeconds();
+    const hourHand = document.getElementById('hour-hand');
+    const minHand = document.getElementById('minute-hand');
+    const secHand = document.getElementById('second-hand');
+    
+    let hourInDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    let minsInDegrees = ((mins / 60) * 360) + ((sec/60)*6) + 90;
+    let secInDegrees = ((sec / 60) * 360) + 90;
+
+    hourHand.style.transform = `rotate(${hourInDegrees}deg)`;
+    minHand.style.transform = `rotate(${minsInDegrees}deg)`;
+    secHand.style.transform = `rotate(${secInDegrees}deg)`;
+}
+
+setInterval(syncDate, 1000);
